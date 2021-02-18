26aab04457b3118e99f8c9ae39a5230c7ea5d4e8
kurapatianurag1 <kurapatianurag1@gmail.com>
2021-02-18 13:31:48 -0500

    Updated commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/image.png">
+    <router-view/>
+      <section>
+        <input placeholder="num1" type="number" class="num" v-model.number="num1"><div class="warning1">Enter number1</div>
+        <select class="num" v-model="operate"> 
+          <option value="+"> + </option>
+          <option value="-"> - </option>
+          <option value="*"> * </option>
+          <option value="/"> / </option>
+        </select>
+        <input placeholder="num2" type="number" class="num" v-model.number="num2"><div class="warning2">Enter number2</div>
+        <button class="cli" v-on:click="press">Result</button>
+        <h2>Output:{{result}}</h2>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      result:''
+    }
+  },
+
+  methods: {
+    // this is calculate part.
+    press(){
+      switch(this.operate) {
+      case "+":
+        return this.result = this.num1 + this.num2
+      case "-":
+        return this.result = this.num1 - this.num2
+      case "*":
+        return this.result = this.num1 * this.num2
+      case "/":
+        return this.result = this.num1 / this.num2
+    }
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.num{
+  position: relative;
+  padding: 5px;
+  border-radius: 4px;
+}
+.warning1{
+  position: absolute;
+  margin-left: 37%;
+  display: none;
+}
+.warning2{
+  position: absolute;
+  margin-left: 50%;
+  display: none;
+}
+.cli{
+  padding: 5px;
+  background:black;
+  color: aliceblue;
+  border-radius: 4px;
 }
 </style>
